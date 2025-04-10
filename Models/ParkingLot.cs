822d167150e9455001f571af465d0e72dd483d00
Khank2104 <nnkhanh159@gmail.com>
2025-04-10 13:55:12 +0700

    Models class cac thuoc tinh nhu thiet ke

---
+++ Models/ParkingLot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkingReportApp.Models
+
+{
+    public class ParkingLot
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Chỉ dòng này là tự tăng
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Capacity { get; set; }
+
+        public virtual ICollection<Ticket> Tickets { get; set; }
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingReportApp.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        public DateTime CheckInTime { get; set; } // BỔ SUNG DÒNG NÀY
+
+        public int ParkingLotId { get; set; }
+        public virtual ParkingLot ParkingLot { get; set; }
+    }
+}
