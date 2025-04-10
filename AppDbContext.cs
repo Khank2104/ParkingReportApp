02101f2a484af901dfa80c4baef134962889f9b1
Khank2104 <nnkhanh159@gmail.com>
2025-04-10 13:58:34 +0700

    code entity blinding ra bang csdl

---
+++ AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using System.Data.Entity;
+using ParkingReportApp.Models;
+
+namespace ParkingReportApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("DefaultConnection")
+        {
+        }
+
+        public AppDbContext(DbConnection connection, bool contextOwnsConnection)
+            : base(connection, contextOwnsConnection)
+        {
+        }
+
+        public DbSet<ParkingLot> ParkingLots { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
